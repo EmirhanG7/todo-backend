2ddfa224d0722ebd27303d0504f69251c7c2e279
Emirhan Gözüküçük <emirhan_g7@outlook.com>
2025-04-22 01:02:08 +0300

    cors ssl hatası

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://todo-frontend.vercel.app'],
+  origin: ['http://localhost:5173', 'https://todo-frontend-blond.vercel.app'],
   credentials: true
 }));
 
