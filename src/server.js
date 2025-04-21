9199f09bf9c122220c32d34f80b265d322d88cb8
Emirhan Gözüküçük <emirhan_g7@outlook.com>
2025-04-22 00:56:18 +0300

    cors hatası

--- src/server.js
+++ src/server.js
@@ -8,8 +8,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://todo-frontend.vercel.app'],
+  credentials: true
+}));
+
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
 
